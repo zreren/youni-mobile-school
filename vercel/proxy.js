4c231e715009b94d175093c2a33dda515f6f5f23
zreren <1152691418@qq.com>
2022-12-28 16:05:59 +0800

    vercel proxy

---
+++ vercel/proxy.js
@@ -0,0 +1,17 @@
+const { createProxyMiddleware } = require('http-proxy-middleware')
+
+module.exports = (req, res) => {
+  let target = ''
+
+  // 代理目标地址
+  // xxxxx 替换为你跨域请求的服务器 如： http://baidu.com
+  if (req.url.startsWith('/api')) {
+    // 这里填目标地址
+    target = 'http://47.100.68.42:5001'
+  }
+  // 创建代理对象并转发请求
+  createProxyMiddleware({
+    target,
+    changeOrigin: true,
+  })(req, res)
+}
