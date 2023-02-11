aa7dc10e29f2aaca238ad76dd31cb479d0b47fc7
zreren <1152691418@qq.com>
2023-02-11 19:23:45 +0800

    改成png导出

--- libs/exportImage.ts
+++ libs/exportImage.ts
@@ -2,7 +2,7 @@ import html2canvas from 'html2canvas';
 import domtoimage from 'dom-to-image';
 const exportAsImage = async (element, imageFileName) => {
   // const canvas = await html2canvas(element);
-  const canvas = await domtoimage.toSvg(element);
+  const canvas = await domtoimage.toPng(element);
   // const image = canvas.toDataURL('image/png', 1.0);
   // console.log(image);
   downloadImage(canvas, imageFileName);
