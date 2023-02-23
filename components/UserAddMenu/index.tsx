aea5320b2a3fee9fe6bb7b131de5f7bd4a27aa72
zreren <1152691418@qq.com>
2023-02-23 20:08:25 +0800

    fix:底部菜单问题

--- components/UserAddMenu/index.tsx
+++ components/UserAddMenu/index.tsx
@@ -95,7 +95,7 @@ export default function index(props) {
         <div className='text-[#8C6008] text-xs font-medium'>转闲置</div>
       </div>
       </div>
-      <Subtract className="absolute -bottom-4 w-screen -z-30 object-cover"></Subtract>
+      <Subtract className="absolute -bottom-4 w-screen -z-30  min-w-[380px] object-cover"></Subtract>
     </div>
   );
 }
